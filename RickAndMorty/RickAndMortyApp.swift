711b9dab40bfefb151dd6c65ef57f0d6021ec96b
bullinnyc <i8u11@icloud.com>
2024-07-20 17:03:57 +0700

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Dmitry Kononchuk on 20.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
